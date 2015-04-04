00bdf33c20ecba56623890515381154e14c5b757
Harrison Harnisch <hharnisc@gmail.com>
2015-04-03 17:58:32 -0700

    version bump to 0.1.5

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(name='python-ddp',
-    version='0.1.4',
+    version='0.1.5',
     description='An event driven ddp client',
     long_description=readme,
     license='MIT',
     author='Harrison Harnisch',
     author_email='hharnisc@gmail.com',
     url='https://github.com/hharnisc/python-ddp',
-    keywords = ["meteor", "ddp", "events", "emitter", "node.js", "node", "eventemitter", "event_emitter"],
+    keywords = ["meteor", "ddp", "events", "emitter", "node.js", "node", "eventemitter", "event_emitter", "ejson"],
     classifiers = [
         "Programming Language :: Python",
         "Development Status :: 2 - Pre-Alpha",
